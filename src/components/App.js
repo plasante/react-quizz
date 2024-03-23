a8d32704c33dfcefd66d12461a6159b2d86fbe37
Pierre Lasante <pierre.lasante@videotron.ca>
2024-03-23 10:53:52 -0400

    Created new components folder

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+
+  index: 0,
 };
 
 // By using a reducer we can update many states simultaneously
@@ -41,7 +43,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -63,7 +68,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
